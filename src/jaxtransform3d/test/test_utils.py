77cf20f76136dc68af4b8f3a128c9dfe2323b4f5
Alexander Fabisch <afabisch@googlemail.com>
2025-03-06 08:00:00 +0100

    TST: test norm_vector

--- src/jaxtransform3d/test/test_utils.py
+++ src/jaxtransform3d/test/test_utils.py
@@ -1,12 +1,12 @@
 import jax.numpy as jnp
 import numpy as np
 import pytest
+from numpy.testing import assert_array_almost_equal
 
 import jaxtransform3d.utils as ju
 
 
-def test_norm_vector():
-    """Test normalization of vectors."""
+def test_norm_vectors_0dim():
     rng = np.random.default_rng(0)
     for n in range(1, 6):
         vec = rng.normal(size=n)
@@ -14,7 +14,22 @@ def test_norm_vector():
         assert pytest.approx(jnp.linalg.norm(vec_unit)) == 1
 
 
+def test_norm_vectors_1dim():
+    rng = np.random.default_rng(8381)
+    vec = rng.standard_normal(size=(100, 3))
+    vec_unit = ju.norm_vector(vec)
+    assert_array_almost_equal(np.linalg.norm(vec_unit, axis=1), np.ones(len(vec)))
+
+
+def test_norm_vectors_3dims():
+    rng = np.random.default_rng(8382)
+    vec = rng.standard_normal(size=(8, 2, 8, 3))
+    vec_unit = ju.norm_vector(vec)
+    assert_array_almost_equal(
+        jnp.linalg.norm(vec_unit, axis=-1), jnp.ones(vec_unit.shape[:-1])
+    )
+
+
 def test_norm_zero_vector():
-    """Test normalization of zero vector."""
     normalized = ju.norm_vector(jnp.zeros(3))
     assert jnp.isfinite(jnp.linalg.norm(normalized))
