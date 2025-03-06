49d78d705e249d2aa46fc936961b9bc24174aace
Alexander Fabisch <afabisch@googlemail.com>
2025-03-06 08:00:00 +0100

    Add autodiff example

--- src/jaxtransform3d/rotations/__init__.py
+++ src/jaxtransform3d/rotations/__init__.py
@@ -6,7 +6,11 @@ from ._axis_angle import (
 )
 from ._matrix import compact_axis_angle_from_matrix
 from ._polar_decomp import norm_matrix, robust_polar_decomposition
-from ._quaternion import compose_quaternions, quaternion_conjugate, compact_axis_angle_from_quaternion
+from ._quaternion import (
+    compact_axis_angle_from_quaternion,
+    compose_quaternions,
+    quaternion_conjugate,
+)
 
 __all__ = [
     "matrix_from_compact_axis_angle",
