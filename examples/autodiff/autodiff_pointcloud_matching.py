5ebb1532bde2ba02f637e90a8aeb72e050e9b3d3
Alexander Fabisch <afabisch@googlemail.com>
2025-03-06 08:00:00 +0100

    Parallel optimizations

--- examples/autodiff/autodiff_pointcloud_matching.py
+++ examples/autodiff/autodiff_pointcloud_matching.py
@@ -19,9 +19,10 @@ def error(exp_coords_BA):
     return jnp.mean(jnp.linalg.norm(pointcloud_B - pointcloud_B_expected, axis=1))
 
 
-error_with_grad = jax.jit(jax.value_and_grad(error))
+error_vmap = lambda x: jax.vmap(error, in_axes=(0,))(x).sum()
+error_with_grad = jax.jit(jax.value_and_grad(error_vmap))
 
-exp_coords_BA = jnp.asarray(rng.normal(size=6))
+exp_coords_BA = jnp.asarray(rng.normal(size=(4, 6)))
 
 for it in range(1000):
     e, grad = error_with_grad(exp_coords_BA)
@@ -35,33 +36,38 @@ T_BA_estimated = jt.transform_from_exponential_coordinates(exp_coords_BA)
 print(f"{T_BA=}")
 print(f"{T_BA_estimated=}")
 
-pointcloud_B_estimated = jt.apply_transform(T_BA_estimated, pointcloud_A)
-
-ax = ppu.make_3d_axis(ax_s=2.0)
-ax.scatter(
-    pointcloud_A[:, 0],
-    pointcloud_A[:, 1],
-    pointcloud_A[:, 2],
-    c="k",
-    label="Untransformed",
-)
-ax.scatter(
-    pointcloud_B[:, 0],
-    pointcloud_B[:, 1],
-    pointcloud_B[:, 2],
-    c="r",
-    alpha=0.5,
-    s=50,
-    label="With actual transform",
-)
-ax.scatter(
-    pointcloud_B_estimated[:, 0],
-    pointcloud_B_estimated[:, 1],
-    pointcloud_B_estimated[:, 2],
-    c="b",
-    label="With estimated transform",
-)
-pt.plot_transform(ax=ax, A2B=T_BA, name="Actual", alpha=0.3, lw=5, s=1.2)
-pt.plot_transform(ax=ax, A2B=T_BA_estimated, name="Estimated")
-plt.legend()
+plt.figure(figsize=(12, 6))
+axes = [
+    plt.subplot(2, len(exp_coords_BA) // 2, 1 + i, projection="3d")
+    for i in range(len(exp_coords_BA))
+]
+for ax, T_BA_estimated_i in zip(axes, T_BA_estimated):
+    pointcloud_B_estimated = jt.apply_transform(T_BA_estimated_i, pointcloud_A)
+    ax.scatter(
+        pointcloud_A[:, 0],
+        pointcloud_A[:, 1],
+        pointcloud_A[:, 2],
+        c="k",
+        label="Untransformed",
+    )
+    ax.scatter(
+        pointcloud_B[:, 0],
+        pointcloud_B[:, 1],
+        pointcloud_B[:, 2],
+        c="r",
+        alpha=0.5,
+        s=50,
+        label="With actual transform",
+    )
+    ax.scatter(
+        pointcloud_B_estimated[:, 0],
+        pointcloud_B_estimated[:, 1],
+        pointcloud_B_estimated[:, 2],
+        c="b",
+        label="With estimated transform",
+    )
+    pt.plot_transform(ax=ax, A2B=T_BA, name="Actual", alpha=0.3, lw=5, s=1.2)
+    pt.plot_transform(ax=ax, A2B=T_BA_estimated_i, name="Estimated")
+    plt.legend()
+plt.tight_layout()
 plt.show()
