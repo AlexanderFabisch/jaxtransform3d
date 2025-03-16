187db1b9741139e8f586f1fd72d55a00afacf1a1
Alexander Fabisch <afabisch@googlemail.com>
2025-03-16 21:53:42 +0100

    Format example

--- examples/vis/vis_poe.py
+++ examples/vis/vis_poe.py
@@ -13,12 +13,11 @@ import chex
 import jax.numpy as jnp
 import jax.random
 import numpy as np
-
 import pytransform3d.transformations as pt
 import pytransform3d.visualizer as pv
 from pytransform3d.urdf import UrdfTransformManager
-import jaxtransform3d.transformations as jt
 
+import jaxtransform3d.transformations as jt
 
 # %%
 # Robot Kinematics
@@ -108,17 +107,13 @@ def get_screw_axes(
         elif joint_type == "prismatic":
             h = np.inf
         else:
-            raise NotImplementedError(
-                "Joint type %s not supported." % joint_type
-            )
+            raise NotImplementedError(f"Joint type {joint_type} not supported.")
 
         screw_axis = pt.screw_axis_from_screw_parameters(q, s_axis, h)
         screw_axes_home.append(screw_axis)
     screw_axes_home = np.vstack(screw_axes_home)
 
-    joint_limits = jnp.array(
-        [tm.get_joint_limits(jn) for jn in joint_names]
-    )
+    joint_limits = jnp.array([tm.get_joint_limits(jn) for jn in joint_names])
 
     return tm, jnp.asarray(ee2base_home), jnp.asarray(screw_axes_home), joint_limits
 
@@ -166,15 +161,15 @@ def product_of_exponentials(ee2base_home, screw_axes_home, joint_limits, thetas)
 
 # %%
 # Then we define a callback to animate the visualization.
-def animation_callback(
-    step, tm, graph, target_frame, joint_names
-):
+def animation_callback(step, tm, graph, target_frame, joint_names):
     global key, target, thetas
 
     if step == 0:
         key, sampling_key = jax.random.split(key, 2)
         current_exp_coords = forward(thetas)
-        exp_coords = 0.9 * current_exp_coords + jnp.array([0.5] * 3 + [0.1] * 3) * jax.random.normal(sampling_key, shape=(6,))
+        exp_coords = 0.9 * current_exp_coords + jnp.array(
+            [0.5] * 3 + [0.1] * 3
+        ) * jax.random.normal(sampling_key, shape=(6,))
         target = jt.transform_from_exponential_coordinates(exp_coords)
         target_frame.set_data(A2B=target)
 
@@ -182,12 +177,16 @@ def animation_callback(
     J = jac(thetas)
     J_inv = jnp.linalg.pinv(J)
     error = jt.exponential_coordinates_from_transform(target) - forward(thetas)
-    new_thetas = thetas + 0.2 * J_inv @ error + 0.05 * (jnp.eye(len(thetas)) - J_inv @ J) @ -thetas
+    new_thetas = (
+        thetas
+        + 0.2 * J_inv @ error
+        + 0.05 * (jnp.eye(len(thetas)) - J_inv @ J) @ -thetas
+    )
     if not jnp.any(jnp.isnan(new_thetas)):
         thetas = jnp.clip(new_thetas, joint_limits[:, 0], joint_limits[:, 1])
     print(thetas)
 
-    for joint_name, value in zip(joint_names, thetas):
+    for joint_name, value in zip(joint_names, thetas, strict=False):
         tm.set_joint(joint_name, value)
     graph.set_data()
 
@@ -201,19 +200,16 @@ def animation_callback(
 BASE_DIR = "data/"
 data_dir = BASE_DIR
 search_path = "."
-while (
-    not os.path.exists(data_dir)
-    and os.path.dirname(search_path) != "jaxtransform3d"
-):
+while not os.path.exists(data_dir) and os.path.dirname(search_path) != "jaxtransform3d":
     search_path = os.path.join(search_path, "..")
     data_dir = os.path.join(search_path, BASE_DIR)
 filename = os.path.join(data_dir, "robot_with_visuals.urdf")
-with open(filename, "r") as f:
+with open(filename) as f:
     robot_urdf = f.read()
 
 # %%
 # define the kinematic chain that we are interested in,
-joint_names = ["joint%d" % i for i in range(1, 7)]
+joint_names = [f"joint{i}" for i in range(1, 7)]
 tm, ee2base_home, screw_axes_home, joint_limits = get_screw_axes(
     robot_urdf, "tcp", "linkmount", joint_names
 )
@@ -233,7 +229,7 @@ jac = jax.jit(jax.jacobian(forward))
 # %%
 # and define the joint angles.
 thetas = -0.5 * jnp.ones(6)
-for joint_name, theta in zip(joint_names, thetas):
+for joint_name, theta in zip(joint_names, thetas, strict=False):
     tm.set_joint(joint_name, theta)
 key = jax.random.PRNGKey(42)
 
